33bf43be4eb019077f1e9073231761f2c50acbeb
diaxen <diaxen@d9d7f853-3bda-4333-babc-cfc2b7b7fb99>
2013-02-17 21:24:54 +0000

    updated email address in copyright

--- trunk/extra/out-skeleton/out_reloc.c
+++ trunk/extra/out-skeleton/out_reloc.c
@@ -16,7 +16,7 @@
 **    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 **
 **    Copyright Alexandre Becoulet, 2002
-**    contact : alexandre.becoulet@epita.fr
+**    contact : alexandre.becoulet@free.fr
 */
 
 #include <stdlib.h>
--- trunk/libcore/oper_b_xor.c
+++ trunk/libcore/oper_b_xor.c
@@ -16,7 +16,7 @@
 **    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 **
 **    Copyright Alexxorre Becoulet, 2002
-**    contact : alexxorre.becoulet@epita.fr
+**    contact : alexxorre.becoulet@free.fr
 */
 
 #include <stdlib.h>
